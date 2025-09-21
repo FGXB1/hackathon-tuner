13f49983acaee6c7ab17083f27134314c9c54c70
Ishan Shah <ishanshah175@gmail.com>
2025-09-21 06:05:16 -0400

    Co-authored-by: Avani Kabra <avanikabra09@users.noreply.github.com> Co-authored-by: Yuvraj832 <Yuvraj832@users.noreply.github.com>

--- main.py
+++ main.py
@@ -12,15 +12,79 @@ import math
 from scipy.io.wavfile import write
 import wave
 import asyncio
-from nicegui import ui
+from nicegui import tailwind, ui
 
 
 record = False
 
+dark = ui.dark_mode()
+dark.enable()
+close_note = "A2"
+global i, desired
+i = 0
+
+record_button = ui.button('Record', on_click=lambda: toggle_record()).style('margin: auto; width: 50%;')
+
+with ui.card().classes('relative w-64 h-32 bg-blue-200'):
+    
+    note_icon = ui.icon('music_note', size='100px').style('margin: auto; width: 50%;').classes('absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-yellow-400 p-2')
+
+
+
+ui.add_head_html('''
+    <style>
+        @keyframes slideLeft {
+            100% { transform: translateX(-80%); }
+        } 
+
+        @keyframes slideRight {
+            100% { transform: translateX(80%); }
+        }
+        
+        @keyframes slideMiddle {
+            100% { transform: translateX(0%); }
+        }
+
+        .slide-left {       
+            animation: slideLeft 2s;
+            animation-fill-mode: forwards;
+        }
+                          
+        .slide-right {
+            animation: slideRight 2s;
+            animation-fill-mode: forwards;
+        }
+        
+        .slide-middle {
+            animation: slideMiddle 2s;
+            animation-fill-mode: forwards;
+        }
+        
+        .slide-across-left {
+            animation: slideAcrossLeft 2s;
+            animation-fill-mode: forwards;
+        }
+                 
+        .slide-across-right {
+            animation: slideAcrossRight 2s;
+            animation-fill-mode: forwards;
+        }
+                 
+    </style>
+''')
+
+def main():
+    
+    with ui.row().style('margin: auto; margin-top: 10px; width: 50%; justify-content: center; align-items: center;'):
+        # left_select.value = ["E2", "A2", "D3"]
+        left_select = ui.radio(["E2", "A2", "D3"]).bind_value(globals(), 'desired')
+        note_label = ui.label(close_note).style('margin: auto; margin-top: 10px; text-align: center; font-size: 100px;')
+        # right_select.value = ["G3", "B3", "E4"]
+        right_select = ui.radio(["G3", "B3", "E4"]).bind_value(globals(), 'desired')
+    ui.button('Submit', on_click=lambda: pitch_indentification(note_icon)).style('margin: auto; width: 50%;')
 
 def toggle_record():
     global record
-    global i
     i=0
     record = not record
     ui.label(record)
@@ -33,7 +97,6 @@ def toggle_record():
         return
     
         
-record_button = ui.button('Record', on_click=lambda: toggle_record())
 
 ui.run()
 
@@ -44,6 +107,7 @@ notes = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
 def detect_pitch(fundamental_frequency):
     semitone_freq = int(np.round(np.log2(fundamental_frequency/concert_pitch)*12)) 
     close_note = notes[semitone_freq%12] + str(abs(4+(semitone_freq+9) // 12))
+    note_label.text = close_note
     close_pitch = concert_pitch*2**(semitone_freq/12)
     return close_note, close_pitch
 
@@ -91,53 +155,118 @@ def find_fundamental(data, sample_rate):
 def audio_record():
     sample_rate = 44100
     duration = 3        
-    ui.label("Recording audio")
+    # ui.label("Recording audio")
     recording = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=1)
     sd.wait()
-    ui.label("Recording finished")
+    # ui.label("Recording finished")
     write("anything.wav", sample_rate, recording)
 
-def desired_pitch(desired, close_note):
+def set_animation(note_icon, new_dir):
+    if new_dir == "middle":
+        note_icon.classes(replace="slide-middle")
+        return
+
+    if new_dir == "left":
+        # if note_icon.classes.contains("slide-right"):
+        #     note_icon.classes(replace="slide-across-left")
+        # else:
+        note_icon.classes(replace='slide-middle')
+        note_icon.classes(replace="slide-left")
+    elif new_dir == "right":
+        # if note_icon.classes.contains("slide-left"):
+        #     note_icon.classes(replace="slide-across-right")
+        # else:
+        note_icon.classes(replace='slide-middle')
+        note_icon.classes(replace="slide-right")
+
+
+
+def desired_pitch(desired, close_note, note_icon):
+    dict = {"C":1, "C#":2, "D":3, "D#":4, "E":5, "F":6, "F#":7, "G":8, "G#":9, "A":10, "A#":11, "B":12}
+    
     if len(desired) == 2:
         dchar = desired[0]
-        dnumber = desired[1]
-    elif len(desired) == 3: 
-        dchar = desired[0]
-        dnumber = desired[2]
+        dnumber = int(desired[1])
+    elif len(desired) == 3:
+        dchar = desired[0:2]
+        dnumber = int(desired[2])
     if len(close_note) == 2:
         cchar = close_note[0]
-        cnumber = close_note[1]
+        cnumber = int(close_note[1])
     elif len(close_note) == 3:
-        cchar = close_note[0]
-        cnumber = close_note[2]
-    
+        cchar = close_note[0:2]
+        cnumber = int(close_note[2])
     if desired == close_note:
-        ui.label("In Tune")
+        ui.notify("In Tune")
+        set_animation(note_icon, "middle")
         return True
-    elif dnumber < cnumber:
-        ui.label("Tune Lower")
+    
+    if dnumber < cnumber:
+        ui.notify("Tune Down")
+        set_animation(note_icon, "right")
     elif dnumber > cnumber: 
-        ui.label("Tune Higher")
+        ui.notify("Tune Up")
+        set_animation(note_icon, "left")
     else:
-        if dchar < cchar:
-            ui.label("Tune Lower")
-        elif dchar > cchar:
-            ui.label("Tune Higher")
+        if dict[dchar] < dict[cchar]:
+            ui.notify("Tune Down")
+            set_animation(note_icon, "right")
         else:
-            if len(desired) > len(close_note):
-                ui.label("Tune Higher")
-            elif len(desired) < len(close_note):
-                ui.label("Tune Lower")
+            ui.notify("Tune Up")
+            set_animation(note_icon, "left")
     return False
 
-def main():
-    global desired
-    ui.input("Enter desired note (E2-E4):").bind_value(globals(), 'desired')
-    ui.button('Submit', on_click=lambda: pitch_indentification())
+    
+    # if len(desired) == 2:
+    #     dchar = desired[0]
+    #     dnumber = desired[1]
+    # elif len(desired) == 3: 
+    #     dchar = desired[0]
+    #     dnumber = desired[2]
+    # if len(close_note) == 2:
+    #     cchar = close_note[0]
+    #     cnumber = close_note[1]
+    # elif len(close_note) == 3:
+    #     cchar = close_note[0]
+    #     cnumber = close_note[2]
+    
+    # if desired == close_note:
+    #     ui.notify("In Tune")
+    #     note_icon.classes(replace ='')
+    #     note_icon.classes(add='slide-middle')
+    #     return True
+    # elif dnumber < cnumber:
+    #     ui.notify("Tune Lower")
+    #     note_icon.classes(replace = 'slide-middle')
+    #     note_icon.classes(add='slide-right')
+    # elif dnumber > cnumber: 
+    #     ui.notify("Tune Higher")
+    #     note_icon.classes(replace='')
+    #     note_icon.classes(add='slide-left')
+    # else:
+    #     if dchar < cchar:
+    #         ui.notify("Tune Lower")
+    #         note_icon.classes(replace ='slide-middle')
+    #         note_icon.classes(add='slide-right')
+    #     elif dchar > cchar:
+    #         ui.notify("Tune Higher")
+    #         note_icon.classes(replace = 'slide-middle')
+    #         note_icon.classes(add='slide-left')
+    #     else:
+    #         if len(desired) > len(close_note):
+    #             ui.notify("Tune Higher")
+    #             note_icon.classes(replace = 'slide-middle')
+    #             note_icon.classes(add='slide-left')
+    #         elif len(desired) < len(close_note):
+    #             ui.notify("Tune Lower")
+    #             note_icon.classes(replace ='slide-middle')
+    #             note_icon.classes(add='slide-right')
+    # return False
+
 
 
     
-async def pitch_indentification():
+async def pitch_indentification(music_note):
     global in_tune 
     in_tune = False
     while not in_tune:
@@ -150,9 +279,9 @@ async def pitch_indentification():
         # ui.notify("detect_pitch()")
         close_note, close_pitch = detect_pitch(fundamental_frequency)
 
-        ui.notify(f"You're playing a {close_note}")
+        
         # ui.notify(f"Close Pitch: {close_pitch}")
-        in_tune = desired_pitch(desired, close_note)
+        in_tune = desired_pitch(desired, close_note, music_note)
         if in_tune:
             ui.notify("In Tune", color='positive')
         await asyncio.sleep(0.1)
